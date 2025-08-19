2140fd9244ebdc9230ffb502ad2b94294234afb1
Hissamr <mohamedhisam1100@gmail.com>
2025-08-19 11:27:45 +0530

    modify: Remove the try and catch block from controller because everything is controlled by GlobalExceptionHandler also add a document for the controller methods.

--- src/main/java/com/bookmind/controller/WishListController.java
+++ src/main/java/com/bookmind/controller/WishListController.java
@@ -30,9 +30,8 @@ public class WishListController {
     
     /**
      * Get all wishlists for a specific user
-     * 
      * @param userId the ID of the user whose wishlists to retrieve
-     * @return a list of wishlist DTOs
+     * @return a List of WishListResponse DTOs
      */
     @GetMapping("/wishlists")
     public ResponseEntity<List<WishListResponse>> getAllWishList(@PathVariable Long userId) {
@@ -43,87 +42,82 @@ public class WishListController {
 
     /**
      * Get a single wishlist by ID
-     * 
      * @param userId the ID of the user who owns the wishlist
      * @param wishListId the ID of the wishlist to retrieve
-     * @return the wishlist DTO
+     * @return the WishListResponse DTO
      */
     @GetMapping("/wishlists/{wishListId}")
     public ResponseEntity<WishListResponse> getWishListById(@PathVariable Long userId, @PathVariable Long wishListId) {
-        try {
-            GetWishListRequest request = new GetWishListRequest(userId, wishListId);
-            WishListResponse response = wishListService.getWishListByUserId(request);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        GetWishListRequest request = new GetWishListRequest(userId, wishListId);
+        WishListResponse response = wishListService.getWishListByUserId(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     /**
      * Create a new wishlist for a user
-     * 
      * @param userId the ID of the user who will own the wishlist
      * @param request the wishlist creation request
-     * @return the created wishlist DTO
+     * @return the WishListResponse DTO
      */
     @PostMapping("/wishlists")
-    public ResponseEntity<?> addWishList(@PathVariable Long userId, @RequestBody CreateWishListRequest request) {
-        try {
-            // Convert entity to response DTO
-            WishListResponse response = wishListService.addWishListToUser(userId, request);
-            // Return the created wishlist response
-            return new ResponseEntity<>(response, HttpStatus.CREATED);
-        }
-        catch (RuntimeException e) {
-            return new ResponseEntity<>("Error adding WishList: " + e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        catch (Exception e) {
-            return new ResponseEntity<>("Error adding WishList: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<WishListResponse> addWishList(@PathVariable Long userId, @RequestBody CreateWishListRequest request) {
+        WishListResponse response = wishListService.addWishListToUser(userId, request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
+    /**
+     * Update the Wishlist of the user
+     * @param userId ID of the User
+     * @param wishListId ID of the WishList to be updated
+     * @param wishList WishList object to be updated
+     * @return WishListResponse
+     */
     @PutMapping("/wishlists/{wishListId}")
-    public ResponseEntity<?> updateWishList(@PathVariable Long userId, @PathVariable Long wishListId, @RequestBody WishList wishList) {
-        try {
-            UpdateWishListRequest request = new UpdateWishListRequest(userId, wishListId, wishList.getName());
-            WishListResponse response = wishListService.updateWishListToUser(request);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>("Error updating wishlist: " + e.getMessage(), HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<WishListResponse> updateWishList(@PathVariable Long userId, @PathVariable Long wishListId, @RequestBody WishList wishList) {
+        UpdateWishListRequest request = new UpdateWishListRequest(userId, wishListId, wishList.getName());
+        WishListResponse response = wishListService.updateWishListToUser(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * Delete a Wishlist of the User
+     * @param userId ID of the User
+     * @param wishListId ID of the WishList to be deleted
+     * @return SuccessResponse indicating the deletion was successful
+     */
     @DeleteMapping("/wishlists/{wishListId}")
-    public ResponseEntity<?> deleteWishList(@PathVariable Long userId, @PathVariable Long wishListId) {
-        try {
-            DeleteWishListRequest request = new DeleteWishListRequest(userId, wishListId);
-            SuccessResponse response = wishListService.deleteWishList(request);
-            return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>("Error deleting wishlist: " + e.getMessage(), HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<SuccessResponse> deleteWishList(@PathVariable Long userId, @PathVariable Long wishListId) {
+        DeleteWishListRequest request = new DeleteWishListRequest(userId, wishListId);
+        SuccessResponse response = wishListService.deleteWishList(request);
+        return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
     }
 
+    /**
+     * Add a book to a Wishlist
+     * @param userId ID of the User
+     * @param wishListId ID of the Wishlist to which the book will be added
+     * @param bookId ID of the Book to be added
+     * @return SuccessResponse indicating the book was added successfully
+     */
     @PostMapping("/wishlists/{wishlistId}/books/{bookId}")
-    public ResponseEntity<?> addBookToWishList(@PathVariable Long userId, @PathVariable Long wishListId, @PathVariable Long bookId) {
-        try {
-            AddBookToWishListRequest request = new AddBookToWishListRequest(userId, wishListId, bookId);
-            SuccessResponse response = wishListService.addBookToWishList(request);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>("Error adding book to wishlist: " + e.getMessage(), HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<SuccessResponse> addBookToWishList(@PathVariable Long userId, @PathVariable Long wishListId, @PathVariable Long bookId) {
+        AddBookToWishListRequest request = new AddBookToWishListRequest(userId, wishListId, bookId);
+        SuccessResponse response = wishListService.addBookToWishList(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * Remove a book from a Wishlist
+     * @param userId ID of the User
+     * @param wishListId ID of the Wishlist from which the book will be removed
+     * @param bookId ID of the Book to be removed
+     * @return SuccessResponse indicating the book was removed successfully
+     */
     @DeleteMapping("/wishlists/{wishlistId}/books/{bookId}")
-    public ResponseEntity<?> removeBookFromWishList(@PathVariable Long userId, @PathVariable Long wishlistId, @PathVariable Long bookId) {
-        try {
-            RemoveBookFromWishListRequest request = new RemoveBookFromWishListRequest(userId, wishlistId, bookId);
-            SuccessResponse response = wishListService.removeBookFromWishList(request);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>("Error removing book from wishlist: " + e.getMessage(), HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<SuccessResponse> removeBookFromWishList(@PathVariable Long userId, @PathVariable Long wishlistId, @PathVariable Long bookId) {
+        RemoveBookFromWishListRequest request = new RemoveBookFromWishListRequest(userId, wishlistId, bookId);
+        SuccessResponse response = wishListService.removeBookFromWishList(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
     
 }
